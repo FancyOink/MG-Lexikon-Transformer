17ccdd8a210aa7b4190e2e4423757becd26bc4cb
Johannes Kuhn <54399034+FancyOink@users.noreply.github.com>
2023-11-06 14:09:18 +0100

    moved old grammars

---
+++ Lexikontransformator/grammars_old/albanisch.pl
@@ -0,0 +1,39 @@
+startCategory(cFin).
+[dy] :: ([c1, -al, -dl]).
+[një] :: ([c1, -bl, -dl]).
+[tre] :: ([c1, -bl]).
+[katër] :: ([c1, -bl]).
+[pesë] :: ([c1, -bl, -el]).
+[gjashtë] :: ([c1, -bl, -el]).
+[shtatë] :: ([c1, -bl, -el]).
+[tetë] :: ([c1, -bl, -el]).
+[nëntë] :: ([c1, -bl, -el]).
+[qind] :: ([=cnix, =c1, c3]).
+[qind_e] :: ([=c3fl, =c1, c3]).
+[dhjetë] :: ([c2, -sl0, -fl]).
+[mbëdhjete] :: ([=c1, +al, c2, -sl0]).
+[mbëdhjetë] :: ([=c1, +bl, c2, -sl0]).
+[tridhjetë] :: ([c2, -sl0, -fl]).
+[tridhjetë_e] :: ([=c1, c2, -sl0]).
+[zet] :: ([=c1, +dl, c2, -sl0]).
+[zet_e] :: ([=c1, =c2dl, c2, -sl0]).
+[dhjetë] :: ([=c1, +el, c2, -sl0]).
+[dhjetë_e] :: ([=c1, =c2el, c2, -sl0]).
+[] :: ([=c3, cFin]).
+[] :: ([=c1, +dl, c2dl]).
+[] :: ([=c2, +fl, c3fl]).
+[] :: ([=c2, +sl0, c3]).
+[] :: ([=c1, +el, c2el]).
+[] :: ([=c1, c3]).
+[] :: ([=c1, c2, -fl]).
+[] :: ([=c1, +dl, c1]).
+[] :: ([=c2, +fl, c2]).
+[] :: ([=c1, +bl, c1]).
+[] :: ([=c1, +el, c1]).
+[] :: ([=c2, +sl0, c2]).
+[] :: ([=c1, +al, c1]).
+[] :: ([=c1, +al, +dl, c1, -al]).
+[] :: ([=c1, +bl, +el, c1, -bl]).
+[] :: ([=c2, +sl0, +fl, c2, -sl0]).
+[] :: ([=c1, +bl, +dl, c1, -bl]).
+[] :: ([cnix]).
---
+++ Lexikontransformator/grammars_old/numbers_trans.pl
@@ -0,0 +1,105 @@
+startCategory(c4(_)).
+[y] :: [=c1,=cee,c3].%[y] :: [=c1,=cee,c2], [epsLi([=c2,c3],dot)]
+[y] :: [=c1,=cee,c4].%[y] :: [=c1,=cee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[y] :: [=c1,=cee,c2].
+[y] :: [=c1,+ee,c3].%[y] :: [=c1,+ee,c2], [epsLi([=c2,c3],dot)]
+[y] :: [=c1,+ee,c4].%[y] :: [=c1,+ee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[y] :: [=c1,+ee,c2].
+[ty] :: [=c1,=ctee,c3].%[ty] :: [=c1,=ctee,c2], [epsLi([=c2,c3],dot)]
+[ty] :: [=c1,=ctee,c4].%[ty] :: [=c1,=ctee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[ty] :: [=c1,=ctee,c2].
+[ty] :: [=c1,+tee,c3].%[ty] :: [=c1,+tee,c2], [epsLi([=c2,c3],dot)]
+[ty] :: [=c1,+tee,c4].%[ty] :: [=c1,+tee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[ty] :: [=c1,+tee,c2].
+[two] :: [c4].%[two] :: [c1], [epsLi([=c2,c4],dot),epsLi([=c1,c4],dot),epsLi([=c1,c2],dot),epsLi([=c2,c3],dot),epsLi([=c3,c4],dot)]
+[two] :: [c3].%[two] :: [c1], [epsLi([=c1,c2],dot),epsLi([=c2,c3],dot)]
+[two] :: [c2].%[two] :: [c1], [epsLi([=c1,c2],dot)]
+[two] :: [c1].
+[twenty] :: [=c1,c3].%[twenty] :: [=c1,c2], [epsLi([=c2,c3],dot)]
+[twenty] :: [=c1,c4].%[twenty] :: [=c1,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[twenty] :: [=c1,c2].
+[twenty] :: [c3].%[twenty] :: [c2], [epsLi([=c2,c3],dot)]
+[twenty] :: [c4].%[twenty] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[twenty] :: [c2].
+[twelve] :: [c3].%[twelve] :: [c2], [epsLi([=c2,c3],dot)]
+[twelve] :: [c4].%[twelve] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[twelve] :: [c2].
+[three] :: [c4].%[three] :: [c1], [epsLi([=c2,c4],dot),epsLi([=c1,c4],dot),epsLi([=c1,c2],dot),epsLi([=c2,c3],dot),epsLi([=c3,c4],dot)]
+[three] :: [c3].%[three] :: [c1], [epsLi([=c1,c2],dot),epsLi([=c2,c3],dot)]
+[three] :: [c2].%[three] :: [c1], [epsLi([=c1,c2],dot)]
+[three] :: [c1].
+[thousand] :: [=cnix,=c3,c4].
+[thousand] :: [=c3,=c3,c4].
+[thirty] :: [=c1,c3].%[thirty] :: [=c1,c2], [epsLi([=c2,c3],dot)]
+[thirty] :: [=c1,c4].%[thirty] :: [=c1,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[thirty] :: [=c1,c2].
+[thirty] :: [c3].%[thirty] :: [c2], [epsLi([=c2,c3],dot)]
+[thirty] :: [c4].%[thirty] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[thirty] :: [c2].
+[thirteen] :: [c3].%[thirteen] :: [c2], [epsLi([=c2,c3],dot)]
+[thirteen] :: [c4].%[thirteen] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[thirteen] :: [c2].
+[ten] :: [c3].%[ten] :: [c2], [epsLi([=c2,c3],dot)]
+[ten] :: [c4].%[ten] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[ten] :: [c2].
+[teen] :: [=c1,+tee,c3].%[teen] :: [=c1,+tee,c2], [epsLi([=c2,c3],dot)]
+[teen] :: [=c1,+tee,c4].%[teen] :: [=c1,+tee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[teen] :: [=c1,+tee,c2].
+[six] :: [c4].%[six] :: [c1,-tee], [epsLi([=c1,c2],dot),epsLi([=c3,c4],dot),epsLi([=c2,c3],dot),epsLi([=c1,c4],dot),epsLi([=c1,+tee,c1],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c4],dot)]
+[six] :: [c3].%[six] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c3],dot)]
+[six] :: [ctee].%[six] :: [c1,-tee], [epsLi([=c1,+tee,ctee],dot)]
+[six] :: [c1].%[six] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot)]
+[six] :: [c2].%[six] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot)]
+[six] :: [c1,-tee].
+[seven] :: [c4].%[seven] :: [c1,-tee], [epsLi([=c1,c2],dot),epsLi([=c3,c4],dot),epsLi([=c2,c3],dot),epsLi([=c1,c4],dot),epsLi([=c1,+tee,c1],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c4],dot)]
+[seven] :: [c3].%[seven] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c3],dot)]
+[seven] :: [ctee].%[seven] :: [c1,-tee], [epsLi([=c1,+tee,ctee],dot)]
+[seven] :: [c1].%[seven] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot)]
+[seven] :: [c2].%[seven] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot)]
+[seven] :: [c1,-tee].
+[one] :: [c4].%[one] :: [c1], [epsLi([=c2,c4],dot),epsLi([=c1,c4],dot),epsLi([=c1,c2],dot),epsLi([=c2,c3],dot),epsLi([=c3,c4],dot)]
+[one] :: [c3].%[one] :: [c1], [epsLi([=c1,c2],dot),epsLi([=c2,c3],dot)]
+[one] :: [c2].%[one] :: [c1], [epsLi([=c1,c2],dot)]
+[one] :: [c1].
+[nine] :: [c4].%[nine] :: [c1,-tee], [epsLi([=c1,c2],dot),epsLi([=c3,c4],dot),epsLi([=c2,c3],dot),epsLi([=c1,c4],dot),epsLi([=c1,+tee,c1],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c4],dot)]
+[nine] :: [c3].%[nine] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c3],dot)]
+[nine] :: [ctee].%[nine] :: [c1,-tee], [epsLi([=c1,+tee,ctee],dot)]
+[nine] :: [c1].%[nine] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot)]
+[nine] :: [c2].%[nine] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot)]
+[nine] :: [c1,-tee].
+[hundredand] :: [=c2,=c1,c4].%[hundredand] :: [=c2,=c1,c3], [epsLi([=c3,c4],dot)]
+[hundredand] :: [=c2,=c1,c3].
+[hundred] :: [=cnix,=c1,c4].%[hundred] :: [=cnix,=c1,c3], [epsLi([=c3,c4],dot)]
+[hundred] :: [=cnix,=c1,c3].
+[four] :: [c4].%[four] :: [c1,-tee], [epsLi([=c1,c2],dot),epsLi([=c3,c4],dot),epsLi([=c2,c3],dot),epsLi([=c1,c4],dot),epsLi([=c1,+tee,c1],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c4],dot)]
+[four] :: [c3].%[four] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot),epsLi([=c2,c3],dot)]
+[four] :: [ctee].%[four] :: [c1,-tee], [epsLi([=c1,+tee,ctee],dot)]
+[four] :: [c1].%[four] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot)]
+[four] :: [c2].%[four] :: [c1,-tee], [epsLi([=c1,+tee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c1,+tee,c2],dot)]
+[four] :: [c1,-tee].
+[five] :: [c4].%[five] :: [c1], [epsLi([=c2,c4],dot),epsLi([=c1,c4],dot),epsLi([=c1,c2],dot),epsLi([=c2,c3],dot),epsLi([=c3,c4],dot)]
+[five] :: [c3].%[five] :: [c1], [epsLi([=c1,c2],dot),epsLi([=c2,c3],dot)]
+[five] :: [c2].%[five] :: [c1], [epsLi([=c1,c2],dot)]
+[five] :: [c1].
+[fifty] :: [=c1,c3].%[fifty] :: [=c1,c2], [epsLi([=c2,c3],dot)]
+[fifty] :: [=c1,c4].%[fifty] :: [=c1,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[fifty] :: [=c1,c2].
+[fifty] :: [c3].%[fifty] :: [c2], [epsLi([=c2,c3],dot)]
+[fifty] :: [c4].%[fifty] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[fifty] :: [c2].
+[fifteen] :: [c3].%[fifteen] :: [c2], [epsLi([=c2,c3],dot)]
+[fifteen] :: [c4].%[fifteen] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[fifteen] :: [c2].
+[eleven] :: [c3].%[eleven] :: [c2], [epsLi([=c2,c3],dot)]
+[eleven] :: [c4].%[eleven] :: [c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[eleven] :: [c2].
+[eight] :: [c3].%[eight] :: [c1,-ee], [epsLi([=c1,+ee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c2,c3],dot)]
+[eight] :: [c4].%[eight] :: [c1,-ee], [epsLi([=c1,c4],dot),epsLi([=c1,+ee,c1],dot),epsLi([=c1,c2],dot),epsLi([=c2,c4],dot)]
+[eight] :: [c2].%[eight] :: [c1,-ee], [epsLi([=c1,+ee,c1],dot),epsLi([=c1,c2],dot)]
+[eight] :: [c1].%[eight] :: [c1,-ee], [epsLi([=c1,+ee,c1],dot)]
+[eight] :: [cee].%[eight] :: [c1,-ee], [epsLi([=c1,+ee,cee],dot)]
+[eight] :: [c1,-ee].
+[een] :: [=c1,+ee,c3].%[een] :: [=c1,+ee,c2], [epsLi([=c2,c3],dot)]
+[een] :: [=c1,+ee,c4].%[een] :: [=c1,+ee,c2], [epsLi([=c2,c3],dot),epsLi([=c3,c4],dot),epsLi([=c2,c4],dot)]
+[een] :: [=c1,+ee,c2].
+[] :: [cnix].
